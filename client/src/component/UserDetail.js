1e3d7d843ce36dc2bddac1276ddb5dc481b82187
“MajerlinNunezB.” <majenunezb@gmail.com>
2021-12-16 00:28:18 -0500

    Maje

--- client/src/component/UserDetail.js
+++ client/src/component/UserDetail.js
@@ -65,7 +65,7 @@ const UserDetail = () => {
             <PhotoDiv>
               <Img src={oneUser?.avatarUrl} />
             </PhotoDiv>
-            {!oneUser?.friends.includes(currentUser._id) ? (
+            {currentUser.friends.includes(oneUser?._id) ? (
               <>
                 <Info>
                   <Name>{oneUser?.name}</Name>
@@ -201,14 +201,17 @@ const FriendList = styled.h1`
   display: flex;
   align-items: center;
   justify-content: center;
+  opacity: 0.6;
 `;
 
 const Button = styled.button`
   align-items: center;
-  margin-left: 180px;
+  margin-left: 470px;
+  margin-top: 40px;
+  margin-bottom: 60px;
   @extend .center-content;
   padding: 10px 30px;
-  font-size: 24px;
+  font-size: 30px;
   cursor: pointer;
 
   border-radius: 9px;
@@ -216,6 +219,7 @@ const Button = styled.button`
 
   background-color: var(--feedback-secondary-color);
   color: #fff;
+  background-color: blue;
   opacity: 0.6;
 
   transition: all 0.3s;
@@ -223,7 +227,7 @@ const Button = styled.button`
   &:hover {
     border-radius: 0px;
 
-    color: #fff;
+    color: #60435f;
     opacity: 0.6;
     background-color: var(--background-color);
     background-image: url("data:image/svg+xml,%3Csvg width='52' height='26' viewBox='0 0 52 26' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23ffacac' fill-opacity='0.4'%3E%3Cpath d='M10 10c0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6h2c0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4v2c-3.314 0-6-2.686-6-6 0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6zm25.464-1.95l8.486 8.486-1.414 1.414-8.486-8.486 1.414-1.414z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
